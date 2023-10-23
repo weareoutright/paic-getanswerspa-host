b718f80a3f9b4ff664b073747caf4d211ebc672f
Dakota Fabro <dakota@weareoutright.com>
2023-10-23 17:28:58 -0400

    Style quotes component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -10,13 +10,17 @@ const QUOTE = {
 const Quote = () => {
   return (
     <section className="Quote">
-      <div className="open-quotation">,,</div>
-      <div className="main-quote">
-        <p className="quote-content">{QUOTE.content}</p>
-        <p className="quote-author">{QUOTE.author}</p>
-        <small className="quote-author-info">{QUOTE.author_info}</small>
+      <div className="open-and-quote">
+        <div className="open-quotation">,,</div>
+        <div className="main-quote">
+          <p className="quote-content">{QUOTE.content}</p>
+          <p className="quote-author">
+            <code>&#8212;</code> {QUOTE.author}
+          </p>
+          <small className="quote-author-info">{QUOTE.author_info}</small>
+        </div>
+        <div className="close-quotation">,,</div>
       </div>
-      <div className="close-quotation">,,</div>
     </section>
   );
 };
