5fde2dd74ed26e49aafe43ab1f30b931509f0d16
Dakota Fabro <dakota@weareoutright.com>
2023-10-23 23:10:02 -0400

    Add styling to Resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,11 +1,34 @@
 import React from "react";
 import SingleResource from "./SingleResource";
+import {
+  MAIN_RESOURCES,
+  FLU_RESOURCES,
+  COVID_RESOURCES,
+} from "../constants/resources";
 
 const Resources = () => {
   return (
     <section className="Resources" id="resources">
       <h2>Resources</h2>
-      <SingleResource />
+      <div className="main-resources">
+        {MAIN_RESOURCES.content.map((resource) => {
+          return <SingleResource key={resource.id} resource={resource} />;
+        })}
+      </div>
+
+      <h4>Flu/Influenza</h4>
+      <div className="flu-resources">
+        {FLU_RESOURCES.content.map((resource) => {
+          return <SingleResource key={resource.id} resource={resource} />;
+        })}
+      </div>
+
+      <h4>Covid-19</h4>
+      <div className="covid-resources">
+        {FLU_RESOURCES.content.map((resource) => {
+          return <SingleResource key={resource.id} resource={resource} />;
+        })}
+      </div>
     </section>
   );
 };
--- src/components/SingleResource.js
+++ src/components/SingleResource.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const SingleResource = () => {
-  return <div className="SingleResource">A single resource goes here</div>;
+const SingleResource = ({ resource }) => {
+  return (
+    <button className="SingleResource">
+      <span className="resource-link">
+        {resource.link.length > 0 ? resource.link : <br></br>}
+      </span>
+      <br />
+      <span className="resource-title">{resource.title}</span>
+    </button>
+  );
 };
 
 export default SingleResource;
