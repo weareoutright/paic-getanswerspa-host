bbd746a2566e168082b521b6f74d28842d149aab
Dakota Fabro <dakota@weareoutright.com>
2023-10-24 12:55:25 -0400

    Add href links to resource buttons

--- src/components/SingleResource.js
+++ src/components/SingleResource.js
@@ -1,15 +1,21 @@
+import { Link } from "gatsby";
 import React from "react";
 
 const SingleResource = ({ resource, topic }) => {
   return (
-    <button className="SingleResource">
-      <span className="resource-link">{topic}</span>
-      <span className="resource-link">
-        {resource.link.length > 0 ? resource.link : <br></br>}
-      </span>
-      <br />
-      <span className="resource-title">{resource.title}</span>
-    </button>
+    <form
+      className="SingleResourceForm"
+      action={resource.link}
+      method="get"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <button className="SingleResource">
+        <span className="resource-link">{topic}</span>
+        <br />
+        <span className="resource-title">{resource.title}</span>
+      </button>
+    </form>
   );
 };
 
