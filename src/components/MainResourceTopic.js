79a247150a4cba32d408c217e1f4187d999af07a
Dakota Fabro <dakota@weareoutright.com>
2023-10-27 15:02:33 -0400

    Decrease leading in section headers

--- src/components/MainResourceTopic.js
+++ src/components/MainResourceTopic.js
@@ -4,14 +4,14 @@ import SingleResource from "./SingleResource";
 const MainResourceTopic = ({ mainResource }) => {
   return (
     <>
-      <h4>{mainResource.title}</h4>
+      <h4>{mainResource.topic}</h4>
       <div className={`main-resources ${mainResource.title}`}>
         {mainResource.content.map((resource) => {
           return (
             <SingleResource
               key={resource.id}
               resource={resource}
-              topic={resource.title}
+              topic={mainResource.topic}
             />
           );
         })}
